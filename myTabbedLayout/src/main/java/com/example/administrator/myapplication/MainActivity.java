d36dff2a6d991b76e54210cd0639f085fe7d0a02
zhanfang1399 <851879081@qq.com>
2016-10-12 16:12:15 +0800

    ffff

--- myTabbedLayout/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ myTabbedLayout/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.administrator.myapplication;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
@@ -10,8 +14,15 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,43 +37,156 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager viewpager;
 
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int mOSVersion = Build.VERSION.SDK_INT;
+        //沉浸式
+        if (mOSVersion >= Build.VERSION_CODES.KITKAT) {
+            Log.i("------","11---------------");
+            setTranslucentStatus(true);
+        }
 
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            Log.i("--------","22---------------");
+            int color = getResources().getColor(R.color.yellow);
+            ViewGroup contentView = (ViewGroup) findViewById(android.R.id.content);
+
+                color = getResources().getColor(R.color.yellow);
+            View statusBarView = new View(this);
+            ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                    getStatusBarHeight(this));
+            statusBarView.setBackgroundColor(color);
+            contentView.addView(statusBarView, lp);
+        }
+
+
+        if (mOSVersion >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            Log.i("------","333---------------");
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+
+
+        setStatusBarDarkMode(true);
 
         assignViews();
-
         setToolBar();
 
         setUpViewPager();
 
 
+    }
 
+    public static int getStatusBarHeight(Context context)
+    {
+        int result = 0;
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0)
+        {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
     }
 
 
 
-    public void setToolBar(){
+
+    /**
+     * 设置状态条透明
+     *
+     * @param on
+     */
+    @TargetApi(19)
+    private void setTranslucentStatus(boolean on) {
+        Window win = getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
+    }
+
+
+    /**
+     * 设置状态栏图标和文本是否为深色
+     * 注:仅支持小米和魅族
+     */
+    protected void setStatusBarDarkMode(boolean darkMode) {
+        setStatusBarDarkModeForMUI(darkMode);
+        setStatusBarDarkModeForMZ(darkMode);
+    }
+
+    private void setStatusBarDarkModeForMUI(boolean darkmode) {
+        Class<? extends Window> clazz = this.getWindow().getClass();
+        try {
+            int darkModeFlag = 0;
+            Class<?> layoutParams = Class.forName("android.view.MiuiWindowManager$LayoutParams");
+            Field field = layoutParams.getField("EXTRA_FLAG_STATUS_BAR_DARK_MODE");
+            darkModeFlag = field.getInt(layoutParams);
+            Method extraFlagField = clazz.getMethod("setExtraFlags", int.class, int.class);
+            extraFlagField.invoke(this.getWindow(), darkmode ? darkModeFlag : 0, darkModeFlag);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setStatusBarDarkModeForMZ(boolean darkmode) {
+        Window window = this.getWindow();
+        if (window != null) {
+            try {
+                WindowManager.LayoutParams lp = window.getAttributes();
+                Field darkFlag = WindowManager.LayoutParams.class.getDeclaredField("MEIZU_FLAG_DARK_STATUS_BAR_ICON");
+                Field meizuFlags = WindowManager.LayoutParams.class.getDeclaredField("meizuFlags");
+                darkFlag.setAccessible(true);
+                meizuFlags.setAccessible(true);
+                int bit = darkFlag.getInt(null);
+                int value = meizuFlags.getInt(lp);
+                if (darkmode) {
+                    value |= bit;
+                } else {
+                    value &= ~bit;
+                }
+                meizuFlags.setInt(lp, value);
+                window.setAttributes(lp);
+            } catch (NoSuchFieldException e) {
+//                LogUtils.e("MeiZu", "setStatusBarDarkIcon: failed");
+            } catch (IllegalAccessException e) {
+//                LogUtils.e("MeiZu", "setStatusBarDarkIcon: failed");
+            }
+        }
+    }
+
+    public void setToolBar() {
 
         setSupportActionBar(toolBar);
         getSupportActionBar().setTitle("hello world");
+//        getSupportActionBar().setSplitBackgroundDrawable(getResources().getDrawable(R.mipmap.ic_launcher));
+//        getSupportActionBar().setStackedBackgroundDrawable(getResources().getDrawable(R.mipmap.ic_launcher));
+        toolBar.setNavigationIcon(getResources().getDrawable(R.mipmap.ic_launcher));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 
-
-    public void setUpViewPager(){
-        ViewPagerAdapter adapter=new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFrag(new TabFragment(),"选项1");
-        adapter.addFrag(new TabFragment(),"选项2");
-        adapter.addFrag(new TabFragment(),"选项3");
+    public void setUpViewPager() {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFrag(new TabFragment(), "选项1");
+//        adapter.addFrag(new TabFragment(), "选项2");
+//        adapter.addFrag(new TabFragment(), "选项3");
         viewpager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewpager);
         coordinatorLayout.setTitleEnabled(false);
+        imageView.setBackgroundResource(R.mipmap.icon_luanch_bottom);
     }
-
 
 
     static class ViewPagerAdapter extends FragmentPagerAdapter {
@@ -94,7 +218,6 @@ public class MainActivity extends AppCompatActivity {
             return mFragmentTitleList.get(position);
         }
     }
-
 
 
     private void assignViews() {
--- myTabbedLayout/src/main/java/com/example/administrator/myapplication/TabFragment.java
+++ myTabbedLayout/src/main/java/com/example/administrator/myapplication/TabFragment.java
@@ -19,8 +19,8 @@ public class TabFragment extends Fragment {
 
     private ListAdapter mAdapter;
 
-    private String mItemData = "Lorem Ipsum is simply dummy text of the printing and "
-            + "typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+    private String mItemData = "Lorem Ipsum is"
+            + " simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
